2205b49efe0bb28f260a899944d01cb4dbe4ee20
Paul Giron <paul.giron@iotic-labs.com>
2016-06-12 16:31:44 +0100

    fix js path

--- home.php
+++ home.php
@@ -15,13 +15,11 @@
 	<link rel="stylesheet" href="css/home.min.css">
 
 	<!-- LIB -->
-	<!-- <script src="common/js/lib/font-awesome.js"></script> -->
-	<!-- <script src="https://use.fontawesome.com/a5176dbf70.js"></script> -->
-	<!-- // <script src="http://code.jquery.com/jquery-latest.js"></script> -->
-	<!-- // <script src="../common/js/jquery-2.1.1.min.js"></script> -->
-	<!-- // <script src="../common/js/jquery-ui-1.11.4.js"></script> -->
-	<!-- // <script src="../common/js/svg_img.js"></script> -->
-	<!-- // <script src="../common/js/bootstrap-full.min.js"></script> -->
+	<script src="common/js/lib/font-awesome.js"></script>
+	<script src="common/js/lib/jquery-2.1.1.min.js"></script>
+	<!-- <script src="common/js/lib/jquery-ui-1.11.4.js"></script> -->
+	<!-- <script src="common/js/lib/svg_img.js"></script> -->
+	<!-- <script src="common/js/lib/bootstrap-full.min.js"></script> -->
 
 	<!-- JS -->
 	<script src="common/js/common.js"></script>
